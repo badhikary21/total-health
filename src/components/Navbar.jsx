ecdcc0a080970a6838b46da6155e305ab8b611d9
Sayan Biswas <sayancyan@gmail.com>
2024-10-03 16:57:43 +0530

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className="bg-black text-white text-center text-5xl py-5">
+        Total Health
+      </h1>
+    </div>
+  );
+}
