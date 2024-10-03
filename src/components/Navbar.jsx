20a59a06d5c1dcf0568937cff557eb22a018f95a
badhikary21 <badhikary767@gmail.com>
2024-10-03 17:14:31 +0530

    red

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div>
-      <h1 className="bg-black text-white text-center text-5xl py-5">
+      <h1 className="bg-red-600 text-white text-center text-5xl py-5">
         Total Health
       </h1>
     </div>
